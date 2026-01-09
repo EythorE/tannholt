071f7c64c5093f5576be02f22518f29285574d6f
EythorE <36506525+EythorE@users.noreply.github.com>
2026-01-09 17:58:23 +0000

    remove preloader

--- js/custom.js
+++ js/custom.js
@@ -2,12 +2,6 @@
 
   "use strict";
 
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').fadeOut(1000); // set duration in brackets    
-    });
-
-
     //Navigation Section
     $('.navbar-collapse a').on('click',function(){
       $(".navbar-collapse").collapse('hide');
